3be852dbdbbe60bc793b3a464b99b63512405937
jinwoen.yang <yjw2288@gmail.com>
2018-05-12 23:38:32 +0900

    first commit java 9 모듈 프로그래밍 책 2.4.6 까지 진행

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -encoding UTF-8 --module-source-path src -d out src/packt.addressbook/packt/addressbook/Main.java  src/packt.addressbook/module-info.java
---
+++ src/packt.addressbook/packt/addressbook/Main.java
@@ -0,0 +1,7 @@
+package packt.addressbook;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println("안녕하세용");
+	}
+}
